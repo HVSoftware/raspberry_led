52426ba8cbdc616875e2928d12b8e5df5a25c6c8
kingrick121 <72559052+kingrick121@users.noreply.github.com>
2021-12-18 19:54:13 +0100

    Create blink.py
    This will blink a led

---
+++ blink.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(27,GPIO.OUT)
+
+print("LED ON")
+GPIO.output(27,GPIO.HIGH)
+time.sleep(2)
+print("LED OFF")
+GPIO.output(27,GPIO.LOW)
+print("LED ON")
+GPIO.output(27,GPIO.HIGH)
+time.sleep(2)
+print("LED OFF")
+GPIO.output(27,GPIO.LOW)
+print("LED ON")
+GPIO.output(27,GPIO.HIGH)
+time.sleep(2)
+print("LED OFF")
+GPIO.output(27,GPIO.LOW)
